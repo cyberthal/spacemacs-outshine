148bfe6bb739a7d1baa618a30cd19672f7157018
Texas Cyberthal <texas.cyberthal@gmail.com>
2017-01-18 11:37:38 +0800

    daily

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 rm -rf ~/.emacs.d/private/outshine
-ln -s ~/1-mansort/git-/hub/emacs/spacemacs/outshine ~/.emacs.d/private/
+ln -s $SCRIPT_DIR ~/.emacs.d/private/
