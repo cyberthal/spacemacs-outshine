a7ecc54714f41c704c92a7f35f06f29dc064734b
leo3 <texas.cyberthal@gmail.com>
2019-07-16 01:05:30 +0800

    replace .spacemacs.d with Spacemacs path

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LINK_NAME="outshine"
 
-mkdir ~/.spacemacs.d/
-rm -f ~/.spacemacs.d/$LINK_NAME
-ln -s $SCRIPT_DIR ~/.spacemacs.d/$LINK_NAME
+mkdir ~/Spacemacs/
+rm -f ~/Spacemacs/$LINK_NAME
+ln -s $SCRIPT_DIR ~/Spacemacs/$LINK_NAME
