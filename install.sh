3fffb690ae72534fdd50c0e2ab65e0628ca20bce
leo3 <texas.cyberthal@gmail.com>
2019-07-17 19:47:51 +0800

    back to original path for layers symlink

--- install.sh
+++ install.sh
@@ -4,6 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LINK_NAME="outshine"
 
-mkdir -p ~/Spacemacs/layers/
-rm -f ~/Spacemacs/layers/$LINK_NAME
-ln -s $SCRIPT_DIR ~/Spacemacs/layers/$LINK_NAME
+rm -f ~/.emacs.d/private/$LINK_NAME
+ln -s $SCRIPT_DIR ~/.emacs.d/private/$LINK_NAME
